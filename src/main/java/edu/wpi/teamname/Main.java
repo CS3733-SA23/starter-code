790d421f1b4d2f115fe7a7793a0e42a9fd5fbbbf
Jay <jcmezzo@wpi.edu>
2023-01-18 17:00:41 -0500

    jay name

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,10 +3,7 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Audrey :)");
     App.launch(App.class, args);
-
-    System.out.println("Hi it's Hazel");
     System.out.println("Jay Mezzo");
   }
 }
