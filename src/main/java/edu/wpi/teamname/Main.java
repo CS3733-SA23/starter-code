7e4e1ea2580cd9031775f2715821f72032734955
Emre <emresbz@gmail.com>
2023-01-17 21:51:45 -0500

    Push

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -7,7 +7,7 @@ public class Main {
     System.out.println("Ian");
     System.out.println("Audrey");
     System.out.println("Owen");
-
+    System.out.println("Emre");
     System.out.println("Jonathan");
 
     App.launch(App.class, args);
