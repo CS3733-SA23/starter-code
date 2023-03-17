7900e0d6d23f9055748fdbdb0f17985e1478e5ad
BJSamurai <40825616+BJSamurai@users.noreply.github.com>
2023-03-17 12:36:34 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package edu.wpi.teamname;
+
+public class Main {
+
+  public static void main(String[] args) {
+    App.launch(App.class, args);
+    System.out.println("Hello, it's Chao here")
+  }
+  // shortcut: psvm
+
+}
