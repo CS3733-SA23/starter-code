8b2e85ec9a35ef385e55a1bef2d1ad027f0521af
BJSamurai <40825616+BJSamurai@users.noreply.github.com>
2023-03-17 12:41:14 -0400

    Add files via upload

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
   public static void main(String[] args) {
     App.launch(App.class, args);
-    System.out.println("Megan Aloise");
-    System.out.println("Albert");
+    System.out.println("Hello, it's Chao here")
   }
   // shortcut: psvm
 
