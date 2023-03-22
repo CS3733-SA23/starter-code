a56abe4ff9dd9ea287da2ce4bcdfb0f6ea6bcf68
Piper O'Connell <proconnell@wpi.edu>
2023-03-21 21:59:30 -0400

    Commit for exercise :)

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,7 +3,7 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Piper O'Connell :)");
+    System.out.println("Piper O'Connell");
     App.launch(App.class, args);
   }
 
