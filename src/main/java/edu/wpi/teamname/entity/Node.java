ccd11ffbe7fc5eef39169bad86da309dcacb25e2
Eddie Stump <ejstump13@gmail.com>
2023-03-25 15:47:56 -0400

    Finished Node.java
    Finished Node.java according to UML diagram

--- src/main/java/edu/wpi/teamname/entity/Node.java
+++ src/main/java/edu/wpi/teamname/entity/Node.java
@@ -3,7 +3,27 @@ package edu.wpi.teamname.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+
 public class Node {
 
+    @Setter @Getter private String id, floor, building, nodeType, longName, shortName;
+    @Setter @Getter private int xCoord, yCoord;
+    @Getter ArrayList<Node> adjacentNodes;
 
+    public Node(String id, String floor, String building, String nodeType, String longName, String shortName,
+                int xCoord, int yCoord) {
+        this.id = id;
+        this.floor = floor;
+        this.building = building;
+        this.nodeType = nodeType;
+        this.longName = longName;
+        this.shortName = shortName;
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+    }
+
+    public void addAdjacent(Node n) {
+        adjacentNodes.add(n);
+    }
 }
