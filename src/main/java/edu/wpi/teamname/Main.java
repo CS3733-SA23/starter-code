51ef348a03485410e611f447bc2778315037113d
Kevin Siegall <digimonlord2@gmail.com>
2023-03-25 12:25:54 -0400

    Test pt 3 lol

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -5,6 +5,8 @@ public class Main {
   public static void main(String[] args) {
     App.launch(App.class, args);
   }
+  // This is a test for the github/discord webhook integration
+  // Remove this in the next commit
 
   // shortcut: psvm
 
