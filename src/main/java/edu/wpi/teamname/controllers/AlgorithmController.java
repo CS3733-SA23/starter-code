99882d6d89547c0a4aea16dc4207e26f7394d9a1
Eddie Stump <ejstump13@gmail.com>
2023-03-25 16:14:41 -0400

    Update AlgorithmController.java
    Wrote outline of AlgorithmController Class

--- src/main/java/edu/wpi/teamname/controllers/AlgorithmController.java
+++ src/main/java/edu/wpi/teamname/controllers/AlgorithmController.java
@@ -1,4 +1,26 @@
 package edu.wpi.teamname.controllers;
 
+import edu.wpi.teamname.entity.Node;
+import java.util.ArrayList;
+
 public class AlgorithmController {
+
+    private ArrayList<Node> nodes;
+
+    public AlgorithmController() {
+        // Add all information from CSV files into the nodes ArrayList, including all edge relationships
+        //TODO: parse CSV and stuff
+    }
+
+    /**
+     * Takes in a starting node and ending node uses breadth first search to find the shortest path between them.
+     *
+     * @param startNodeID node to start the search
+     * @param endNodeID node to search for
+     * @return shortest path from the start to the end node
+     */
+    public ArrayList<Node> breadthfirstSearch(String startNodeID, String endNodeID) {
+        return null;
+        //TODO: Write search algorithm
+    }
 }
