44a178ae0e068be6607b6918ac5a8fa8b5c687cf
randyer <rbdyer3@gmail.com>
2023-03-26 14:47:00 -0400

    added features list

--- src/main/java/edu/wpi/teamname/Cdb.java
+++ src/main/java/edu/wpi/teamname/Cdb.java
@@ -1,6 +1,7 @@
 package edu.wpi.teamname;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Cdb {
 
@@ -17,7 +18,47 @@ public class Cdb {
       connection = DriverManager.getConnection(url, user, password);
 
       // Do something with the connection
-      // ...
+      //
+
+      Scanner scanner = new Scanner(System.in);
+      boolean continueProg = true;
+      while (continueProg) {
+        System.out.println(
+            "Display node and edge information\n"
+                + "Update node coordinates\n"
+                + "Update name of location node\n"
+                + "Export node table into a CSV file\n"
+                + "Import from a CSV file into the node table\n"
+                + "Display Help on how to use your database program\n"
+                + "Exit the program\n");
+        String command = scanner.nextLine();
+        switch (command) {
+          case "Display node and edge information\n":
+            //
+            break;
+          case "Update node coordinates":
+            //
+            break;
+          case "Update name of location node":
+            //
+            break;
+          case "Export node table into a CSV file":
+            //
+            break;
+          case "Import from a CSV file into the node table\n":
+            //
+            break;
+          case "Display Help on how to use your database program":
+            //
+            break;
+          case "Exit the program":
+            continueProg = false;
+            break;
+          default:
+            System.out.println("Command not found");
+            break;
+        }
+      }
 
     } catch (Exception e) {
       e.printStackTrace();
