035996589b6eb4428e2f01142411e11ac1cdabbb
Seth Frank <sbfrank@wpi.edu>
2023-03-26 14:14:24 -0400

    Changed directories from teamname to teamA

---
+++ src/main/java/edu/wpi/teamA/App.java
@@ -0,0 +1,47 @@
+package edu.wpi.teamA;
+
+import edu.wpi.teamA.navigation.Navigation;
+import edu.wpi.teamA.navigation.Screen;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App extends Application {
+
+  @Setter @Getter private static Stage primaryStage;
+  @Setter @Getter private static BorderPane rootPane;
+
+  @Override
+  public void init() {
+    log.info("Starting Up");
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws IOException {
+    /* primaryStage is generally only used if one of your components require the stage to display */
+    App.primaryStage = primaryStage;
+
+    final FXMLLoader loader = new FXMLLoader(App.class.getResource("views/Root.fxml"));
+    final BorderPane root = loader.load();
+
+    App.rootPane = root;
+
+    final Scene scene = new Scene(root);
+    primaryStage.setScene(scene);
+    primaryStage.show();
+
+    Navigation.navigate(Screen.HOME);
+  }
+
+  @Override
+  public void stop() {
+    log.info("Shutting Down");
+  }
+}
