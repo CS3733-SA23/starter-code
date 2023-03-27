ec34ecb32a5e49310672b47b8109dc6d499cea62
Kevin Siegall <digimonlord2@gmail.com>
2023-03-27 13:17:35 -0400

    Refactor to fit class diagram

---
+++ src/main/java/pathfinding/HospitalNode.java
@@ -0,0 +1,37 @@
+package pathfinding;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class HospitalNode {
+    private List<HospitalNode> neighbors;
+    private HashMap<HospitalNode, Integer> edgeCosts;
+    private int cost;
+
+    public HospitalNode() {
+        this.neighbors = new LinkedList<HospitalNode>();
+        edgeCosts = new HashMap<HospitalNode, Integer>();
+    }
+
+    public HospitalNode addNeighbor(HospitalNode neighbor) {
+        neighbors.add(neighbor);
+        edgeCosts.put(neighbor, 1);
+        return this;
+    }
+
+    public HospitalNode addNeighbor(HospitalNode neighbor, int cost) {
+        neighbors.add(neighbor);
+        edgeCosts.put(neighbor, cost);
+        return this;
+    }
+
+    public List<HospitalNode> getNeighbors() {
+        return neighbors;
+    }
+
+    public static void addEdge(HospitalNode node1, HospitalNode node2) {
+        node1.neighbors.add(node2);
+        node2.neighbors.add(node1);
+    }
+}
--- src/main/java/pathfinding/PathfindingMain.java
+++ src/main/java/pathfinding/PathfindingMain.java
@@ -1,7 +1,5 @@
 package pathfinding;
 
 public class PathfindingMain {
-    public static void main(String[] args) {
-
-    }
+  public static void main(String[] args) {}
 }
