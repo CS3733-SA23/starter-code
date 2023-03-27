022b1f038d895c7f66d52b837e5b5b3eb77486ad
randyer <rbdyer3@gmail.com>
2023-03-26 21:07:37 -0400

    added update coordinates functionality

--- src/main/java/edu/wpi/teamname/Cdb.java
+++ src/main/java/edu/wpi/teamname/Cdb.java
@@ -17,74 +17,41 @@ public class Cdb {
       String password = "teamc30";
       connection = DriverManager.getConnection(url, user, password);
 
-      // Do something with the connection
-      //
-
       Scanner scanner = new Scanner(System.in);
+      //////////////
+
       boolean continueProg = true;
-
-      Statement stmtNode = connection.createStatement();
-      Statement stmtEdge = connection.createStatement();
-
-      // table names
-      String node = "\"hospitalNode\".node";
-      String edge = "\"hospitalNode\".edge";
-      // queries
-      String queryDisplayNodes = "SELECT * FROM " + node;
-      String queryDisplayEdges = "SELECT * FROM " + edge;
-
-      ResultSet rsNodes = stmtNode.executeQuery(queryDisplayNodes);
-      ResultSet rsEdges = stmtEdge.executeQuery(queryDisplayEdges);
-
       while (continueProg) {
-        System.out.println(
-            "===========================================\n"
-                + "Display node and edge information\n"
-                + "Update node coordinates\n"
-                + "Update name of location node\n"
-                + "Export node table into a CSV file\n"
-                + "Import from a CSV file into the node table\n"
-                + "Help\n"
-                + "Exit\n"
-                + "===========================================\n");
-        String command = scanner.nextLine();
+        displayInstructions();
+        String command = scanner.nextLine().toLowerCase();
         switch (command) {
-          case "Display node and edge information":
-            try {
-              System.out.println("Node information: \n");
-              while (rsNodes.next()) {
-                for (int i = 1; i <= rsNodes.getMetaData().getColumnCount(); i++) {
-                  System.out.print(rsNodes.getString(i) + "\t");
-                }
-                System.out.println("\n");
-              }
-              System.out.println("Edge information: \n");
-              while (rsEdges.next()) {
-                for (int i = 1; i <= rsEdges.getMetaData().getColumnCount(); i++) {
-                  System.out.print(rsEdges.getString(i) + "\t");
-                }
-                System.out.println("\n");
-              }
-            } catch (Exception e) {
-              e.printStackTrace();
-            }
+          case "display node and edge information":
+            // displays all of the node and edge attributes
+            displayNodeAndEdgeInfo(connection);
             break;
-          case "Update node coordinates":
+          case "update node coordinates":
+            System.out.println("please type the nodeID of the node you would like to update:");
+            String nodeID = scanner.nextLine();
+            System.out.println("Enter new x coordinate:");
+            int xCoordinate = scanner.nextInt();
+            System.out.println("Enter new y coordinate:");
+            int yCoordinate = scanner.nextInt();
+            // update the coordinates with given nodeID and values
+            updateCoordinates(connection, xCoordinate, yCoordinate, nodeID);
+            break;
+          case "update name of location node":
             //
             break;
-          case "Update name of location node":
+          case "export node table into a csv file":
             //
             break;
-          case "Export node table into a CSV file":
+          case "import from a csv file into the node table\n":
             //
             break;
-          case "Import from a CSV file into the node table\n":
-            //
+          case "help":
+            System.out.println("");
             break;
-          case "Help":
-            //
-            break;
-          case "Exit":
+          case "exit":
             continueProg = false;
             break;
           default:
@@ -104,6 +71,78 @@ public class Cdb {
           e.printStackTrace();
         }
       }
+    }
+  }
+
+  static void displayInstructions() {
+    System.out.println(
+        "===========================================\n"
+            + "Options:\n"
+            + "Display node and edge information\n"
+            + "Update node coordinates\n"
+            + "Update name of location node\n"
+            + "Export node table into a CSV file\n"
+            + "Import from a CSV file into the node table\n"
+            + "Help\n"
+            + "Exit\n"
+            + "===========================================\n");
+  }
+
+  static void displayNodeAndEdgeInfo(Connection connection) {
+    try {
+      Statement stmtNode = connection.createStatement();
+      Statement stmtEdge = connection.createStatement();
+      // table names
+      String node = "\"hospitalNode\".node";
+      String edge = "\"hospitalNode\".edge";
+      // queries
+      String queryDisplayNodes = "SELECT * FROM " + node;
+      String queryDisplayEdges = "SELECT * FROM " + edge;
+
+      ResultSet rsNodes = stmtNode.executeQuery(queryDisplayNodes);
+      ResultSet rsEdges = stmtEdge.executeQuery(queryDisplayEdges);
+      System.out.println("Node information: \n");
+      while (rsNodes.next()) {
+        for (int i = 1; i <= rsNodes.getMetaData().getColumnCount(); i++) {
+          System.out.print(rsNodes.getString(i) + "\t");
+        }
+        System.out.println("\n");
+      }
+      System.out.println(
+          "----------------------------------------------------------------------------------------------------------------------------");
+      System.out.println("Edge information: \n");
+      while (rsEdges.next()) {
+        for (int i = 1; i <= rsEdges.getMetaData().getColumnCount(); i++) {
+          System.out.print(rsEdges.getString(i) + "\t");
+        }
+        System.out.println("\n");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  static void updateCoordinates(
+      Connection connection, int xCoordinate, int yCoordinate, String nodeID) {
+    try {
+      PreparedStatement stmtUpdateCoord =
+          connection.prepareStatement(
+              "UPDATE \"hospitalNode\".node\n"
+                  + "SET xcoord = ?, ycoord = ?\n"
+                  + "WHERE \"nodeID\" = ?;");
+      // set parameters for prepared statement
+      stmtUpdateCoord.setInt(1, xCoordinate);
+      stmtUpdateCoord.setInt(2, yCoordinate);
+      stmtUpdateCoord.setString(3, nodeID);
+
+      int rowsUpdated = stmtUpdateCoord.executeUpdate();
+      if (rowsUpdated > 0) {
+        System.out.println("update successful!");
+      } else {
+        System.out.println("Node not found: Invalid nodeID");
+      }
+    } catch (SQLException e) {
+      System.out.println("SQL exception occurred: " + e.getMessage());
     }
   }
 }
