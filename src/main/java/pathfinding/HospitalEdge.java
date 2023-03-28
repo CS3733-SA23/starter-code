b4752bf91186fcfffb2465ff9ba771553d9f79d1
Kevin Siegall <digimonlord2@gmail.com>
2023-03-28 16:20:16 -0400

    Minor parameter name change

--- src/main/java/pathfinding/HospitalEdge.java
+++ src/main/java/pathfinding/HospitalEdge.java
@@ -6,17 +6,17 @@ public class HospitalEdge {
   String nodeTwoID;
   int edgeWeight;
 
-  public HospitalEdge(String edgeID, String n1ID, String n2ID) {
+  public HospitalEdge(String edgeID, String nodeOneID, String nodeTwoID) {
     this.edgeID = edgeID;
-    this.nodeOneID = n1ID;
-    this.nodeTwoID = n2ID;
+    this.nodeOneID = nodeOneID;
+    this.nodeTwoID = nodeTwoID;
     this.edgeWeight = 1; // Default edge weight of 1
   }
 
-  public HospitalEdge(String edgeID, String n1ID, String n2ID, int weight) {
+  public HospitalEdge(String edgeID, String nodeOneID, String nodeTwoID, int weight) {
     this.edgeID = edgeID;
-    this.nodeOneID = n1ID;
-    this.nodeTwoID = n2ID;
+    this.nodeOneID = nodeOneID;
+    this.nodeTwoID = nodeTwoID;
     this.edgeWeight = weight;
   }
 }
