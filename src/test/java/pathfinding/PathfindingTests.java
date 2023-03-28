c790fb91c3f1111365ee1821c251c2dea13d995b
aarshzadaphiya <aszadaphiya@wpi.edu>
2023-03-28 15:48:06 -0400

    Initial Test

--- src/test/java/pathfinding/PathfindingTests.java
+++ src/test/java/pathfinding/PathfindingTests.java
@@ -17,11 +17,11 @@ public class PathfindingTests {
     HospitalNode node4 = new HospitalNode("4",7,9);
     HospitalNode node5 = new HospitalNode("5",10,10);
     HospitalNode node6 = new HospitalNode("6",12,12);
-    HospitalNode.addEdge(node1, node2);
-    HospitalNode.addEdge(node2, node4);
-    HospitalNode.addEdge(node4, node5);
-    HospitalNode.addEdge(node1, node3);
-    HospitalNode.addEdge(node3, node5);
+    HospitalNode.addEdge(node1, node2, 1);
+    HospitalNode.addEdge(node2, node4,3);
+    HospitalNode.addEdge(node4, node5,1);
+    HospitalNode.addEdge(node1, node3,5);
+    HospitalNode.addEdge(node3, node5,2);
   }
 
   /**
@@ -84,13 +84,58 @@ public class PathfindingTests {
     assertEquals(7, pathfinder.heuristicDistance(node1, node3));
     assertEquals(0, pathfinder.heuristicDistance(node1, node1));
   }
-  public void test1() {
+  @Test
+  public void testAstarMiddleCase1() {
+    createTestGraph();
+    AbstractPathfinder pathfinder = new BFSPathfinder();
+    List<HospitalNode> path = pathfinder.findPath(HospitalNode.allNodes.get("1"), HospitalNode.allNodes.get("5"));
+    assertEquals(path.get(0), HospitalNode.allNodes.get("1"));
+    assertEquals(path.get(1), HospitalNode.allNodes.get("2"));
+    assertEquals(path.get(2), HospitalNode.allNodes.get("4"));
+    assertEquals(path.get(3), HospitalNode.allNodes.get("5"));
+  }
+
+  @Test
+  public void testAstarMiddleCase2() {
+    createTestGraph();
+    AbstractPathfinder pathfinder = new BFSPathfinder();
+    List<HospitalNode> path = pathfinder.findPath(HospitalNode.allNodes.get("4"), HospitalNode.allNodes.get("3"));
+    assertEquals(path.get(0), HospitalNode.allNodes.get("1"));
+    assertEquals(path.get(1), HospitalNode.allNodes.get("5"));
+    assertEquals(path.get(2), HospitalNode.allNodes.get("3"));
+  }
+  @Test
+  public void testAstarMiddleCase3() {
+    createTestGraph();
+    AbstractPathfinder pathfinder = new BFSPathfinder();
+    List<HospitalNode> path = pathfinder.findPath(HospitalNode.allNodes.get("2"), HospitalNode.allNodes.get("5"));
+    assertEquals(path.get(0), HospitalNode.allNodes.get("2"));
+    assertEquals(path.get(1), HospitalNode.allNodes.get("4"));
+    assertEquals(path.get(2), HospitalNode.allNodes.get("5"));
+  }
+  @Test
+  public void testAstarMiddleCase4() {
+    createTestGraph();
+    AbstractPathfinder pathfinder = new BFSPathfinder();
+    List<HospitalNode> path = pathfinder.findPath(HospitalNode.allNodes.get("1"), HospitalNode.allNodes.get("3"));
+    assertEquals(path.get(0), HospitalNode.allNodes.get("1"));
+    assertEquals(path.get(1), HospitalNode.allNodes.get("3"));
+  }
+  @Test
+  public void testAstarMiddleCase5() {
     createTestGraph();
     AbstractPathfinder pathfinder = new BFSPathfinder();
     List<HospitalNode> path = pathfinder.findPath(HospitalNode.allNodes.get("2"), HospitalNode.allNodes.get("3"));
     assertEquals(path.get(0), HospitalNode.allNodes.get("2"));
     assertEquals(path.get(1), HospitalNode.allNodes.get("1"));
     assertEquals(path.get(2), HospitalNode.allNodes.get("3"));
+
+  @Test
+  public void testAstarFailCase() {
+    createTestGraph();
+    AbstractPathfinder pathfinder = new BFSPathfinder();
+    List<HospitalNode> path = pathfinder.findPath(HospitalNode.allNodes.get("1"), HospitalNode.allNodes.get("6"));
+    assertNull(path);
   }
 
 }
