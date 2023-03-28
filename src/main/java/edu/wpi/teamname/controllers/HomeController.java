a64077c04b3cd02524e6b9b1f72ebada0f49a3c3
michibatoryu <114957192+michibatoryu@users.noreply.github.com>
2023-03-28 15:57:00 -0400

    added mouseover events for the main menu buttons

--- src/main/java/edu/wpi/teamname/controllers/HomeController.java
+++ src/main/java/edu/wpi/teamname/controllers/HomeController.java
@@ -1,9 +1,12 @@
 package edu.wpi.teamname.controllers;
 
+import static javafx.scene.paint.Color.*;
+
 import edu.wpi.teamname.navigation.Navigation;
 import edu.wpi.teamname.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
+import javafx.scene.paint.Color;
 
 public class HomeController {
 
@@ -17,10 +20,44 @@ public class HomeController {
   @FXML
   public void initialize() {
 
+    flowersButton.setOnMouseEntered(
+        event -> {
+          flowersButton.setStyle(
+              "-fx-background-color: #ffffff; -fx-alignment: center-left; -fx-border-color: #192d5a; -fx-border-width: 2;");
+          flowersButton.setTextFill(Color.web("#192d5aff", 1.0));
+        });
+    flowersButton.setOnMouseExited(
+        event -> {
+          flowersButton.setStyle("-fx-background-color: #192d5aff; -fx-alignment: center-left;");
+          flowersButton.setTextFill(WHITE);
+        });
     flowersButton.setOnMouseClicked(event -> Navigation.navigate(Screen.FLOWER_REQUEST));
 
-    // navigateButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SERVICE_REQUEST));
+    signageButton.setOnMouseEntered(
+        event -> {
+          signageButton.setStyle(
+              "-fx-background-color: #ffffff; -fx-alignment: center-left; -fx-border-color: #192d5a; -fx-border-width: 2;");
+          signageButton.setTextFill(Color.web("#192d5aff", 1.0));
+        });
+    signageButton.setOnMouseExited(
+        event -> {
+          signageButton.setStyle("-fx-background-color: #192d5aff; -fx-alignment: center-left;");
+          signageButton.setTextFill(WHITE);
+        });
     signageButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SIGNAGE_TEXT));
+
+    mealRequestButton.setOnMouseEntered(
+        event -> {
+          mealRequestButton.setStyle(
+              "-fx-background-color: #ffffff; -fx-alignment: center-left; -fx-border-color: #192d5a; -fx-border-width: 2;");
+          mealRequestButton.setTextFill(Color.web("#192d5aff", 1.0));
+        });
+    mealRequestButton.setOnMouseExited(
+        event -> {
+          mealRequestButton.setStyle(
+              "-fx-background-color: #192d5aff; -fx-alignment: center-left;");
+          mealRequestButton.setTextFill(WHITE);
+        });
     mealRequestButton.setOnMouseClicked(event -> Navigation.navigate(Screen.MEAL_REQUEST));
   }
 }
