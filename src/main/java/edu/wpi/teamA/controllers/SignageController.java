760e3ab7ccdfc264dc5ced671127dd87a8009af5
Ayden Duncan <aydenduncan610@gmail.com>
2023-03-27 21:51:40 -0400

    signage code working

--- src/main/java/edu/wpi/teamA/controllers/SignageController.java
+++ src/main/java/edu/wpi/teamA/controllers/SignageController.java
@@ -1,3 +1,16 @@
 package edu.wpi.teamA.controllers;
 
-public class SignageController {}
+import edu.wpi.teamA.navigation.Navigation;
+import edu.wpi.teamA.navigation.Screen;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.fxml.FXML;
+
+public class SignageController {
+
+  @FXML MFXButton backButton;
+
+  @FXML
+  public void initialize() {
+    backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+  }
+}
