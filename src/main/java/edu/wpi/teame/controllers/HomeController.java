6cf7b643fa794dfd6daed462ba7b54a39d29ac42
michibatoryu <114957192+michibatoryu@users.noreply.github.com>
2023-03-28 17:19:39 -0400

    successful merge
    changed a few more things from "teamname" to "teame"

--- src/main/java/edu/wpi/teame/controllers/HomeController.java
+++ src/main/java/edu/wpi/teame/controllers/HomeController.java
@@ -1,12 +1,11 @@
-package edu.wpi.teamname.controllers;
+package edu.wpi.teame.controllers;
 
 import static javafx.scene.paint.Color.*;
 
-import edu.wpi.teamname.navigation.Navigation;
-import edu.wpi.teamname.navigation.Screen;
+import edu.wpi.teame.navigation.Navigation;
+import edu.wpi.teame.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 
@@ -25,11 +24,11 @@ public class HomeController {
   public void initialize() {
     logoImage.setOnMouseEntered(
         event -> {
-          //change to wong.jpg
+          // change to wong.jpg
         });
     logoImage.setOnMouseExited(
         event -> {
-          //change to teamlogo.jpg
+          // change to teamlogo.jpg
         });
 
     flowersButton.setOnMouseEntered(
