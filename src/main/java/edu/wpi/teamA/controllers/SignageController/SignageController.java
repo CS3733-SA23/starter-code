f06571f6bf36904b113116fd1e2c208fb136bd8d
Seth Frank <sbfrank@wpi.edu>
2023-03-28 22:03:10 -0400

    Changed controller directories

---
+++ src/main/java/edu/wpi/teamA/controllers/SignageController/SignageController.java
@@ -0,0 +1,18 @@
+package edu.wpi.teamA.controllers.SignageController;
+
+import edu.wpi.teamA.navigation.Navigation;
+import edu.wpi.teamA.navigation.Screen;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.fxml.FXML;
+
+public class SignageController {
+
+  @FXML MFXButton backButton;
+
+  @FXML
+  public void initialize() {}
+
+  public void goBack() {
+    backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+  }
+}
