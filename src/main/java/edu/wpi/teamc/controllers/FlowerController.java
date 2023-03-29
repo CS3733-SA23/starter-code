92ec795d91f67a5a2712bf04bf48766aad8687a9
azzhang3 <azhang3@wpi.edu>
2023-03-29 14:15:40 -0400

    Fixed FlowerPage Select Menu - Aaron

--- src/main/java/edu/wpi/teamc/controllers/FlowerController.java
+++ src/main/java/edu/wpi/teamc/controllers/FlowerController.java
@@ -5,11 +5,50 @@ import edu.wpi.teamc.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 
 public class FlowerController {
   @FXML private MFXButton FLOW_backtohome;
   @FXML private MFXButton FLOW_submit;
   @FXML private Label FLOW_outputtext;
+
+  @FXML private MenuItem chocie0;
+
+  @FXML private MenuItem chocie1;
+
+  @FXML private MenuItem chocie2;
+
+  @FXML private MenuItem chocie3;
+
+  @FXML private MenuItem choice4;
+
+  @FXML private MenuButton menuButton;
+
+  @FXML
+  void getChoice0() {
+    menuButton.setText("--Please Select Bouquet Option--");
+  }
+
+  @FXML
+  void getChoice1() {
+    menuButton.setText("Roses");
+  }
+
+  @FXML
+  void getChoice2() {
+    menuButton.setText("White Roses");
+  }
+
+  @FXML
+  void getChoice3() {
+    menuButton.setText("Random Bouquet");
+  }
+
+  @FXML
+  void getChoice4() {
+    menuButton.setText("Ian's Holiday Special");
+  }
 
   @FXML
   public void goHome() {
