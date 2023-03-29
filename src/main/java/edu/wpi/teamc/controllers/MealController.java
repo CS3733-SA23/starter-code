56b22e873114c5116261abfbce5f4899bdf32a08
BJSamurai <40825616+BJSamurai@users.noreply.github.com>
2023-03-28 21:22:04 -0400

    Service Menubutton added
    Signed-off-by: BJSamurai <40825616+BJSamurai@users.noreply.github.com>

--- src/main/java/edu/wpi/teamc/controllers/MealController.java
+++ src/main/java/edu/wpi/teamc/controllers/MealController.java
@@ -5,11 +5,17 @@ import edu.wpi.teamc.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 
 public class MealController {
   @FXML private MFXButton MEAL_backtohome;
   @FXML private MFXButton MEAL_submit;
   @FXML private Label MEAL_outputtext;
+  @FXML private MenuButton MEAL_Menu;
+  @FXML private MenuItem MEAL_Default;
+  @FXML private MenuItem MEAL_Keto;
+  @FXML private MenuItem MEAL_Vegan;
 
   @FXML
   public void initialize() {
@@ -19,5 +25,20 @@ public class MealController {
   @FXML
   void SubmitRequest() {
     MEAL_outputtext.setText("Submitted");
+  }
+
+  @FXML
+  void MEAL_Choice0() {
+    MEAL_Menu.setText("-Please Select Meal-");
+  }
+
+  @FXML
+  void MEAL_Choice1() {
+    MEAL_Menu.setText("Keto");
+  }
+
+  @FXML
+  void MEAL_Choice2() {
+    MEAL_Menu.setText("Vegan");
   }
 }
--- src/main/java/edu/wpi/teamc/controllers/OfficeSupplyController.java
+++ src/main/java/edu/wpi/teamc/controllers/OfficeSupplyController.java
@@ -5,11 +5,17 @@ import edu.wpi.teamc.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 
 public class OfficeSupplyController {
   @FXML private MFXButton OFF_backtohome;
   @FXML private MFXButton OFF_submit;
   @FXML private Label OFF_outputtext;
+  @FXML private MenuButton OFF_Menu;
+  @FXML private MenuItem OFF_Default;
+  @FXML private MenuItem OFF_Pen;
+  @FXML private MenuItem OFF_Notebook;
 
   @FXML
   public void initialize() {
@@ -19,5 +25,20 @@ public class OfficeSupplyController {
   @FXML
   void SubmitRequest() {
     OFF_outputtext.setText("Submitted");
+  }
+
+  @FXML
+  void OFF_Choice0() {
+    OFF_Menu.setText("-Please Select Office Supply-");
+  }
+
+  @FXML
+  void OFF_Choice1() {
+    OFF_Menu.setText("Pen");
+  }
+
+  @FXML
+  void OFF_Choice2() {
+    OFF_Menu.setText("Notebook");
   }
 }
