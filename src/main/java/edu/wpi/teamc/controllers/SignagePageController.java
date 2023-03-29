51fff03ae21fec23afe2252615f2b00a8a653ee0
C-beaus <cabeausoleil@wpi.edu>
2023-03-28 23:18:50 -0400

    fixed function names in controllers to describe where the next location will be once a specific button is pressed (ie going to home page)

--- src/main/java/edu/wpi/teamc/controllers/SignagePageController.java
+++ src/main/java/edu/wpi/teamc/controllers/SignagePageController.java
@@ -3,7 +3,6 @@ package edu.wpi.teamc.controllers;
 import edu.wpi.teamc.navigation.Navigation;
 import edu.wpi.teamc.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 /** */
@@ -16,6 +15,6 @@ public class SignagePageController {
     backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
   }
 
-//  public void backAction(ActionEvent actionEvent) {}
-//  // to use this way, have "backAction" under onAction in .fxml file
+  //  public void backAction(ActionEvent actionEvent) {}
+  //  // to use this way, have "backAction" under onAction in .fxml file
 }
