ba66d5f231eaae247b49c29a559494366984b65d
Nick Ishigami <nickyish13@gmail.com>
2023-03-30 20:19:31 -0400

    fixed edge class

--- src/main/java/edu/wpi/teamc/map/Edge.java
+++ src/main/java/edu/wpi/teamc/map/Edge.java
@@ -5,11 +5,13 @@ import lombok.Getter;
 @Getter
 public class Edge {
   private String id;
+  private Node startNode;
   private Node endNode;
   private int weight;
 
-  public Edge(String id, Node endNode) {
+  public Edge(String id, Node startNode, Node endNode) {
     this.id = id;
+    this.startNode = startNode;
     this.endNode = endNode;
     this.weight = 0;
   }
