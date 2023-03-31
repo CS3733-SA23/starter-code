cdadda8a32208e618a95b0cfd455fdaead5118e5
joeyc5565 <jscaproni@wpi.edu>
2023-03-31 18:59:25 -0400

    Drop down menu modified so text displays when option selected.

--- src/main/java/edu/wpi/teamc/controllers/ConferenceController.java
+++ src/main/java/edu/wpi/teamc/controllers/ConferenceController.java
@@ -2,14 +2,18 @@ package edu.wpi.teamc.controllers;
 
 import edu.wpi.teamc.navigation.Navigation;
 import edu.wpi.teamc.navigation.Screen;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 
 public class ConferenceController {
   @FXML private Button CONF_submit;
   @FXML private Button CONF_backtohome;
   @FXML private Label CONF_outputtext;
+  @FXML private MenuButton RoomSelection;
 
   @FXML
   public void goHome() {
@@ -22,7 +26,24 @@ public class ConferenceController {
   }*/
 
   @FXML
+  void getRoomSelection(ActionEvent event) {}
+
+  @FXML private MenuItem Option1;
+
+  @FXML private MenuItem Option2;
+
+  @FXML
   void getSubmitButton() {
     CONF_submit.setOnMouseClicked(event -> Navigation.navigate(Screen.CONGRATS_PAGE));
+  }
+
+  @FXML
+  void getOption1(ActionEvent event) {
+    RoomSelection.setText("Option 1");
+  }
+
+  @FXML
+  void getOption2(ActionEvent event) {
+    RoomSelection.setText("Option 2");
   }
 }
