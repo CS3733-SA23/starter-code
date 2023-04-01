11439ec2b910e2ba22f61fa1517f623458a885db
Nick Ishigami <nickyish13@gmail.com>
2023-04-01 14:20:10 -0400

    add weight calculation and heuristic function

--- src/main/java/edu/wpi/teamc/map/Edge.java
+++ src/main/java/edu/wpi/teamc/map/Edge.java
@@ -7,13 +7,22 @@ public class Edge {
   private String id;
   private Node startNode;
   private Node endNode;
-  private int weight;
+  private double weight;
+  private double heuristic;
 
   public Edge(String id, Node startNode, Node endNode) {
     this.id = id;
     this.startNode = startNode;
     this.endNode = endNode;
-    this.weight = 0;
+    this.weight = Math.hypot(startNode.getXCoord() - endNode.getXCoord(), startNode.getYCoord() - endNode.getYCoord());
+  }
+
+  /**
+   * Use manhattan distance to set heuristic value
+   * @param targetNode Target destination
+   */
+  public void setHeuristic(Node targetNode) {
+    this.heuristic = Math.hypot(startNode.getXCoord() - targetNode.getXCoord(), startNode.getYCoord() - targetNode.getYCoord());
   }
 }
 
