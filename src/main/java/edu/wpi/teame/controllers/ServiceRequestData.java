2dbbcd1cbee8aac1d70332022aedf6f76dde18d3
Kevin Siegall <digimonlord2@gmail.com>
2023-04-02 16:56:48 -0400

    New enum and staff String

--- src/main/java/edu/wpi/teame/controllers/ServiceRequestData.java
+++ src/main/java/edu/wpi/teame/controllers/ServiceRequestData.java
@@ -1,19 +1,36 @@
 package edu.wpi.teame.controllers;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.json.JSONObject;
 
 public class ServiceRequestData {
-
+  enum Status {
+    PENDING,
+    IN_PROGRESS,
+    DONE
+  }
   enum RequestType {
     MEALDELIVERY,
     FLOWERDELIVERY,
   }
 
-  JSONObject requestData;
-  RequestType requestType;
+  @Getter @Setter JSONObject requestData;
+  @Getter @Setter RequestType requestType;
+  @Getter @Setter Status requestStatus;
+  @Getter @Setter String assignedStaff;
 
   public ServiceRequestData(RequestType requestType, JSONObject requestData) {
     this.requestData = requestData;
     this.requestType = requestType;
+    requestStatus = Status.PENDING;
+    assignedStaff = "None";
+  }
+
+  public ServiceRequestData(RequestType requestType, JSONObject requestData, Status currentStatus, String assignedStaff) {
+    this.requestData = requestData;
+    this.requestType = requestType;
+    requestStatus = Status.PENDING;
+    assignedStaff = "None";
   }
 }
