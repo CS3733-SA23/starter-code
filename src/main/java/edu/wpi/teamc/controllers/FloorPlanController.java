043eccdcf2e505b99c5c0a0fe1129ffe357dcb18
joeyc5565 <jscaproni@wpi.edu>
2023-04-02 19:01:55 -0400

    Floor plan page

--- src/main/java/edu/wpi/teamc/controllers/FloorPlanController.java
+++ src/main/java/edu/wpi/teamc/controllers/FloorPlanController.java
@@ -1,4 +1,41 @@
 package edu.wpi.teamc.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+
 public class FloorPlanController {
+  @FXML private MenuItem choice1;
+
+  @FXML private MenuItem choice2;
+
+  @FXML private MenuItem choice3;
+
+  @FXML private MenuItem choice4;
+
+  @FXML private MenuButton menuButton;
+
+  @FXML
+  void getChoice1(ActionEvent event) {
+    menuButton.setText("Floor 1");
+  }
+
+  @FXML
+  void getChoice2(ActionEvent event) {
+    menuButton.setText("Floor 2");
+  }
+
+  @FXML
+  void getChoice3(ActionEvent event) {
+    menuButton.setText("Floor 3");
+  }
+
+  @FXML
+  void getChoice4(ActionEvent event) {
+    menuButton.setText("Floor 4");
+  }
+
+  @FXML
+  void getMenuButton(ActionEvent event) {}
 }
