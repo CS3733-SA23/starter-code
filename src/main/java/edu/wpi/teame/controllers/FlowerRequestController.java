11263e69f514d052cf1058cd01e2dc18c64edcd5
Megan Jacques <mjacques@wpi.edu>
2023-04-02 16:38:15 -0400

    Re-did Flower Request Page

--- src/main/java/edu/wpi/teame/controllers/FlowerRequestController.java
+++ src/main/java/edu/wpi/teame/controllers/FlowerRequestController.java
@@ -15,12 +15,19 @@ public class FlowerRequestController implements IRequestController {
   ObservableList<String> flowerChoices =
       FXCollections.observableArrayList("Tiger Lilies", "Roses", "Tulips", "Daisies");
 
-  ObservableList<String> flowerNum = FXCollections.observableArrayList("1", "2", "6", "8", "12");
+  ObservableList<String> flowerNum =
+      FXCollections.observableArrayList(
+          "1", "2", "Small Bouquet (6)", "Medium Bouquet (8)", "Large Bouquet (12)");
 
-  @FXML MFXButton cancelButton;
-  @FXML MFXButton submitButton;
+  ObservableList<String> deliveryTimes =
+      FXCollections.observableArrayList(
+          "10am - 11am", "11am - 12pm", "12pm - 1pm", "1pm - 2pm", "2pm - 3pm", "3pm - 4pm");
+
+  @FXML MFXButton returnButtonFlowerRequest;
+  @FXML MFXButton flowerRequestSubmit;
   @FXML MFXTextField recipientName;
   @FXML MFXTextField roomNumber;
+  @FXML MFXComboBox deliveryTime;
   @FXML MFXComboBox flowerChoice;
   @FXML MFXComboBox numOfFlowers;
   @FXML MFXTextField notes;
@@ -29,14 +36,17 @@ public class FlowerRequestController implements IRequestController {
   public void initialize() {
     flowerChoice.setItems(flowerChoices);
     numOfFlowers.setItems(flowerNum);
-    cancelButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SERVICE_REQUESTS));
-    submitButton.setOnMouseClicked(event -> sendRequest());
+    deliveryTime.setItems(deliveryTimes);
+    returnButtonFlowerRequest.setOnMouseClicked(
+        event -> Navigation.navigate(Screen.SERVICE_REQUESTS));
+    flowerRequestSubmit.setOnMouseClicked(event -> sendRequest());
   }
 
   public ServiceRequestData sendRequest() {
     // Get the selected/input values
     String selFlowers = flowerChoice.getText();
     String selNumOfFlowers = numOfFlowers.getText();
+    String selDeliveryTime = deliveryTime.getText();
     String inputName = recipientName.getText();
     String inputRoom = roomNumber.getText();
     String inputNotes = notes.getText();
@@ -45,6 +55,7 @@ public class FlowerRequestController implements IRequestController {
     JSONObject requestData = new JSONObject();
     requestData.put("flowers", selFlowers);
     requestData.put("numFlowers", selNumOfFlowers);
+    requestData.put("deliveryTime", selDeliveryTime);
     requestData.put("recipient", inputName);
     requestData.put("room", inputRoom);
     requestData.put("notes", inputNotes);
