ce9d3b6135086815b8360479caf21cb46487d3ed
michibatoryu <114957192+michibatoryu@users.noreply.github.com>
2023-04-02 14:44:38 -0400

    minor changes

--- src/main/java/edu/wpi/teame/controllers/DatabaseViewController.java
+++ src/main/java/edu/wpi/teame/controllers/DatabaseViewController.java
@@ -1,4 +1,9 @@
 package edu.wpi.teame.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
 public class DatabaseViewController {
+    @FXML
+    TableView<String> dataTable;
 }
