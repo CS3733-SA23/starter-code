0497b836ed9e5b6fb4706381a20b938c448b9a99
michibatoryu <114957192+michibatoryu@users.noreply.github.com>
2023-04-02 13:42:00 -0400

    Utilities class, mouseover method, color enum, etc...

--- src/main/java/edu/wpi/teame/controllers/HomeController.java
+++ src/main/java/edu/wpi/teame/controllers/HomeController.java
@@ -1,15 +1,12 @@
-
 package edu.wpi.teame.controllers;
 
-import static javafx.scene.paint.Color.*;
-
-import edu.wpi.teame.navigation.Navigation;
+import edu.wpi.teame.navigation.ColorPalette;
 import edu.wpi.teame.navigation.Screen;
+import edu.wpi.teame.navigation.Utilities;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
 
 public class HomeController {
   @FXML MFXButton signageButton;
@@ -30,44 +27,16 @@ public class HomeController {
           // change to teamlogo.jpg
         });
 
-    navMouseSetup(exitButton);
+    Utilities.addButtonHover(exitButton);
     exitButton.setOnMouseClicked(event -> Platform.exit());
 
-    mouseSetup(flowersButton);
-    flowersButton.setOnMouseClicked(event -> Navigation.navigate(Screen.FLOWER_REQUEST));
+    Utilities.addButtonHover(flowersButton);
+    flowersButton.setOnMouseClicked(event -> Utilities.navigate(Screen.FLOWER_REQUEST));
 
-    mouseSetup(signageButton);
-    signageButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SIGNAGE_TEXT));
+    Utilities.addButtonHover(signageButton, ColorPalette.DARK_BLUE, ColorPalette.WHITE, true);
+    signageButton.setOnMouseClicked(event -> Utilities.navigate(Screen.SIGNAGE_TEXT));
 
-    mouseSetup(mealRequestButton);
-    mealRequestButton.setOnMouseClicked(event -> Navigation.navigate(Screen.MEAL_REQUEST));
-  }
-
-  private void mouseSetup(MFXButton btn) {
-    btn.setOnMouseEntered(
-        event -> {
-          btn.setStyle(
-              "-fx-background-color: #ffffff; -fx-alignment: center-left; -fx-border-color: #192d5a; -fx-border-width: 2;");
-          btn.setTextFill(Color.web("#192d5aff", 1.0));
-        });
-    btn.setOnMouseExited(
-        event -> {
-          btn.setStyle("-fx-background-color: #192d5aff; -fx-alignment: center-left;");
-          btn.setTextFill(WHITE);
-        });
-  }
-
-  private void navMouseSetup(MFXButton btn) { // for buttons such as exit, back, cancel, etc
-    btn.setOnMouseEntered(
-        event -> {
-          btn.setStyle(
-              "-fx-background-color: #ffffff; -fx-alignment: center; -fx-border-color: #3b63a5; -fx-border-width: 2;");
-          btn.setTextFill(Color.web("#3b63a5", 1.0));
-        });
-    btn.setOnMouseExited(
-        event -> {
-          btn.setStyle("-fx-background-color: #3b63a5; -fx-alignment: center;");
-          btn.setTextFill(WHITE);
-        });
+    Utilities.addButtonHover(mealRequestButton, ColorPalette.DARK_BLUE, ColorPalette.WHITE, true);
+    mealRequestButton.setOnMouseClicked(event -> Utilities.navigate(Screen.MEAL_REQUEST));
   }
 }
---
+++ src/main/java/edu/wpi/teame/navigation/Utilities.java
@@ -0,0 +1,127 @@
+package edu.wpi.teame.navigation;
+
+import edu.wpi.teame.App;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.paint.Color;
+
+public class Utilities {
+  public static void navigate(final Screen screen) {
+    final String filename = screen.getFilename();
+
+    try {
+      final var resource = App.class.getResource(filename);
+      final FXMLLoader loader = new FXMLLoader(resource);
+
+      App.getRootPane().setCenter(loader.load());
+    } catch (IOException | NullPointerException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * adds listeners for entering and exiting a given MFXButton that allow color change on hover (ie
+   * the button and text switch colors)
+   *
+   * @param button the button that is being configured
+   * @param backgroundColor the corresponding ColorPalette entry for the background color of the
+   *     button
+   * @param textColor the corresponding ColorPalette entry for the color of the text in the button
+   */
+  public static void addButtonHover(
+      MFXButton button, ColorPalette backgroundColor, ColorPalette textColor, boolean leftAligned) {
+    addButtonHover(button, backgroundColor.getHexCode(), textColor.getHexCode(), leftAligned);
+  }
+
+  /**
+   * adds listeners for entering and exiting a given MFXButton that allow color change on hover (ie
+   * the button and text switch colors)
+   *
+   * @param button the button that is being configured
+   * @param backgroundHex the hex code for the background of the button (include the hash, eg
+   *     "#ffee00" or "aaff1cff")
+   * @param textHex the hex code for the text in the button (include the hash, eg "#ffee00" or
+   *     "aaff1cff")
+   */
+  public static void addButtonHover(
+      MFXButton button, String backgroundHex, String textHex, boolean leftAligned) {
+    String alignment = leftAligned ? "center-left" : "center";
+
+    button.setOnMouseEntered(
+        event -> {
+          button.setStyle(
+              "-fx-background-color: "
+                  + textHex
+                  + "; -fx-alignment: "
+                  + alignment
+                  + "; -fx-border-color: "
+                  + backgroundHex
+                  + "; -fx-border-width: 2;");
+          button.setTextFill(Color.web(backgroundHex, 1.0));
+        });
+    button.setOnMouseExited(
+        event -> {
+          button.setStyle(
+              "-fx-background-color: " + backgroundHex + "; -fx-alignment: " + alignment + ";");
+          button.setTextFill(Color.web(textHex, 1.0));
+        });
+  }
+
+  /**
+   * adds listeners for entering and exiting a given MFXButton that allow color change on hover (ie
+   * the button and text switch colors)
+   */
+  public static void addButtonHover(MFXButton button) {
+    String textHex = buttonText(button);
+    String alignment = buttonTextAlignment(button);
+    String backgroundHex = buttonBackground(button);
+
+    button.setOnMouseEntered(
+        event -> {
+          button.setStyle(
+              "-fx-background-color: "
+                  + textHex
+                  + "; -fx-alignment: "
+                  + alignment
+                  + "; -fx-border-color: "
+                  + backgroundHex
+                  + "; -fx-border-width: 2;");
+          button.setTextFill(Color.web(backgroundHex, 1.0));
+        });
+    button.setOnMouseExited(
+        event -> {
+          button.setStyle(
+              "-fx-background-color: " + backgroundHex + "; -fx-alignment: " + alignment + ";");
+          button.setTextFill(Color.web(textHex, 1.0));
+        });
+  }
+
+  // Helpers
+
+  private static String buttonBackground(MFXButton button) {
+    String style = button.getStyle();
+    int loc = style.indexOf("-fx-background-color: ");
+    loc += "-fx-background-color: ".length();
+    // System.out.println("location: " + loc);
+    // System.out.println("style length: " + style.length());
+    return style.substring(loc, loc + 7) + "ff";
+  }
+
+  private static String buttonText(MFXButton button) {
+    String text = button.getTextFill().toString();
+    return "#" + text.substring(2);
+  }
+
+  public static String buttonTextAlignment(MFXButton button) {
+    String style = button.getStyle();
+    int loc = style.indexOf("-fx-alignment: ");
+    loc += "-fx-alignment: ".length();
+
+    if (loc == -1) return "center";
+
+    // find the end index of the alignment field
+    int end = style.substring(loc).indexOf(";");
+    return style.substring(loc, loc + end);
+  }
+}
