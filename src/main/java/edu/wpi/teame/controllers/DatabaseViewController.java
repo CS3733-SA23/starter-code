231f573bd882a5bacea10688108b661c11171e72
michibatoryu <114957192+michibatoryu@users.noreply.github.com>
2023-04-02 14:49:53 -0400

    added id's to columns

--- src/main/java/edu/wpi/teame/controllers/DatabaseViewController.java
+++ src/main/java/edu/wpi/teame/controllers/DatabaseViewController.java
@@ -1,9 +1,19 @@
 package edu.wpi.teame.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class DatabaseViewController {
     @FXML
     TableView<String> dataTable;
+
+    @FXML
+    TableColumn nodeIDCol;
+
+    @FXML
+    TableColumn nameCol;
+
+    @FXML
+    TableColumn dateCol;
 }
