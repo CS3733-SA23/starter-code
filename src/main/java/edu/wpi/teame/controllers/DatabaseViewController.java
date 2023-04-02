a9a255e475abe3d97828ce991572e46fe9db4cae
michibatoryu <114957192+michibatoryu@users.noreply.github.com>
2023-04-02 15:12:16 -0400

    datatypes included

--- src/main/java/edu/wpi/teame/controllers/DatabaseViewController.java
+++ src/main/java/edu/wpi/teame/controllers/DatabaseViewController.java
@@ -3,17 +3,36 @@ package edu.wpi.teame.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseViewController {
     @FXML
-    TableView<String> dataTable;
+    TableView<MapInitializer> dataTable;
 
     @FXML
-    TableColumn nodeIDCol;
+    TableColumn<MapInitializer, String> nodeIDCol;
 
     @FXML
-    TableColumn nameCol;
+    TableColumn<MapInitializer, String> nameCol;
 
     @FXML
-    TableColumn dateCol;
+    TableColumn<MapInitializer, String> dateCol;
+
+
+    @FXML
+    public void initialize(){
+        //load the database into the table
+        nodeIDCol.setCellValueFactory(new PropertyValueFactory<MapInitializer,String>("nodeID"));
+        nameCol.setCellValueFactory(new PropertyValueFactory<MapInitializer, String>("longName"));
+        dateCol.setCellValueFactory(new PropertyValueFactory<MapInitializer, String>("date"));
+
+        List itemList = new ArrayList<>(); //REPLACE WITH THE METHOD CALL
+
+        dataTable.setItems();
+
+        dataTable.setEditable(true);
+    }
 }
