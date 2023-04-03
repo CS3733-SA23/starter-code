f180dffaca3b07c0b412a94f81030ec6d67a56e3
Megan Jacques <mjacques@wpi.edu>
2023-04-03 14:36:30 -0400

    Floor 1 & Floor 2 Map Display Changes

--- src/main/java/edu/wpi/teame/controllers/FloorTwoMapNavController.java
+++ src/main/java/edu/wpi/teame/controllers/FloorTwoMapNavController.java
@@ -1,5 +1,7 @@
 package edu.wpi.teame.controllers;
 
+import edu.wpi.teame.navigation.Navigation;
+import edu.wpi.teame.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
 
@@ -21,4 +23,14 @@ public class FloorTwoMapNavController {
 
     @FXML
     private MFXButton lowerLevelOneButton;
+
+    public void initialize()
+    {
+        lowerLevelOneButton.setOnMouseClicked(event -> Navigation.navigate(Screen.LOWER_ONE));
+        lowerLevelTwoButton.setOnMouseClicked(event -> Navigation.navigate(Screen.LOWER_TWO));
+        groundFloorButton.setOnMouseClicked(event -> Navigation.navigate(Screen.GROUND_FLOOR));
+        firstFloorButton.setOnMouseClicked(event -> Navigation.navigate(Screen.FLOOR_ONE));
+        ThirdFloorButton.setOnMouseClicked(event -> Navigation.navigate(Screen.FLOOR_THREE));
+        backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+    }
 }
--- src/main/java/edu/wpi/teame/navigation/Screen.java
+++ src/main/java/edu/wpi/teame/navigation/Screen.java
@@ -6,7 +6,14 @@ public enum Screen {
   SERVICE_REQUEST("views/ServiceRequest.fxml"),
   SIGNAGE_TEXT("views/Signage.fxml"),
   MEAL_REQUEST("views/MealRequest.fxml"),
-  FLOWER_REQUEST("views/FlowerRequest.fxml");
+  FLOWER_REQUEST("views/FlowerRequest.fxml"),
+
+  LOWER_ONE("views/LoweLevelOneMapNav.fxml"),
+  LOWER_TWO("views/LowerLevelTwoMapNav.fxml"),
+  FLOOR_ONE("views/FloorOneMapNav.fxml"),
+  FLOOR_TWO("views/FloorTwoMapNav.fxml"),
+  FLOOR_THREE("views/FloorThreeMapNav.fxml"),
+  GROUND_FLOOR("views/GroundFloorMapForNavigation.fxml");
 
   private final String filename;
 
