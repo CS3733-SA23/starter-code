e40ddacc6e297afce7dc8eb313209273a1a8eff1
Jamie-Rapal <jrapal@wpi.edu>
2023-04-03 14:20:36 -0400

    Added new, smaller retrieveFromTable method in DatabaseGraphController

--- src/main/java/Database/DatabaseGraphController.java
+++ src/main/java/Database/DatabaseGraphController.java
@@ -29,87 +29,43 @@ public class DatabaseGraphController {
     nodeList = new ArrayList<>();
     edgeList = new ArrayList<>();
 
-    List<String> eList = new ArrayList<>();
-    List<String> nList = new ArrayList<>();
+    String queryCountEID = "SELECT * FROM teame.\"Edge\";";
+    String queryCountNID = "SELECT * FROM teame.\"Node\";";
 
-    String queryCountE = "SELECT COUNT(*) FROM teame.l1edges;";
-    String queryCountN = "SELECT COUNT(*) FROM teame.l1nodes;";
-    String queryCountEID = "SELECT edgeID FROM teame.l1edges;";
-    String queryCountNID = "SELECT nodeID FROM teame.l1nodes;";
-
-    try (Statement stmt = DBC.getC().createStatement()) {
-      ResultSet rsn = stmt.executeQuery(queryCountN);
-      if (rsn.next()) {
-        int nodeCount = rsn.getInt(1);
-        ResultSet rsNodes = stmt.executeQuery(queryCountNID);
-        for (int i = 1; i <= nodeCount; i++) {
-          if (rsNodes.next()) {
-            nList.add(rsNodes.getString("nodeID"));
-          }
-        }
+    try {
+      Statement stmt = DBC.getC().createStatement();
+      ResultSet rsn = stmt.executeQuery(queryCountNID);
+      while (rsn.next()) {
+        HospitalNode hn = extractNodeFromResultSet(rsn);
+        nodeList.add(hn);
       }
-      ResultSet rse = stmt.executeQuery(queryCountE);
-      if (rse.next()) {
-        int edgeCount = rse.getInt(1);
-        ResultSet rsEdges = stmt.executeQuery(queryCountEID);
-        for (int i = 1; i <= edgeCount; i++) {
-          if (rsEdges.next()) {
-            String newid = rsEdges.getString("edgeid");
-            eList.add(newid);
-          }
-        }
+
+      ResultSet rse = stmt.executeQuery(queryCountEID);
+      while (rse.next()) {
+        HospitalEdge he = extractEdgeFromResultSet(rse);
+        edgeList.add(he);
       }
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
-
-    // Retrieve edges
-    for (String edgeId : eList) {
-      String edgeQuery = "SELECT * FROM teame.l1edges WHERE edgeid = '" + edgeId + "';";
-      try {
-        Statement stmt = DBC.getC().createStatement();
-        ResultSet rs = stmt.executeQuery(edgeQuery);
-
-        if (rs.next()) {
-          edgeList.add(extractEdgeFromResultSet(rs));
-        }
-      } catch (SQLException m) {
-        System.out.println(m.getMessage());
-      }
-    }
     if (edgeList.isEmpty()) {
-      System.out.println("No edges retrieved for the given list of IDs.");
-    } else {
-      System.out.println("Edges retrieved successfully.");
-    }
-
-    // Retrieve nodes
-    for (String nodeId : nList) {
-      String nodeQuery = "SELECT * FROM teame.l1nodes WHERE nodeid = '" + nodeId + "'";
-      try (Statement stmt = DBC.getC().createStatement()) {
-        ResultSet rs = stmt.executeQuery(nodeQuery);
-        if (rs.next()) {
-          nodeList.add(extractNodeFromResultSet(rs));
-        }
-      } catch (SQLException d) {
-        System.out.println(d.getMessage());
-      }
+      System.out.println("No edges retrieved");
     }
     if (nodeList.isEmpty()) {
-      System.out.println("No nodes retrieved for the given list of IDs");
+      System.out.println("No nodes retrieved");
     } else {
-      System.out.println("Nodes retrieved successfully.");
+      System.out.println("Edges and Nodes retrieved successfully.");
     }
   }
 
   private HospitalNode extractNodeFromResultSet(ResultSet rs) throws SQLException {
-    String nodeID = rs.getString("nodeid");
+    int nodeID = rs.getInt("nodeID");
     int xCoord = rs.getInt("xcoord");
     int yCoord = rs.getInt("ycoord");
     String floor = rs.getString("floor");
     String building = rs.getString("building");
 
-    return new HospitalNode(new NodeInitializer(nodeID, xCoord, yCoord, floor, building));
+    return new HospitalNode(new NodeInitializer(nodeID + "", xCoord, yCoord, floor, building));
   }
 
   private HospitalEdge extractEdgeFromResultSet(ResultSet rs) throws SQLException {
--- src/test/java/Database/DatabaseTest.java
+++ src/test/java/Database/DatabaseTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import pathfinding.Floor;
+import pathfinding.HospitalEdge;
+import pathfinding.HospitalNode;
 import pathfinding.MoveAttribute;
 
 public class DatabaseTest {
@@ -34,5 +36,21 @@ public class DatabaseTest {
 
     System.out.println(moveAttributeList.size());
     assertEquals(0, 0);
+  }
+
+  @Test
+  public void testNewRetrieveFromTable() {
+    DatabaseGraphController DBMC = setup();
+    DBMC.retrieveFromTable();
+
+    List<HospitalNode> nlist = DBMC.getHospitalNodes();
+    List<HospitalEdge> elist = DBMC.getHospitalEdges();
+
+    for (HospitalNode hn : nlist) {
+      System.out.println(hn);
+    }
+
+    assertEquals(581, nlist.size());
+    assertEquals(684, elist.size());
   }
 }
