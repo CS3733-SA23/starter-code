07780569d63fe08c7514222e6cc107ee34c87a1d
Megan Jacques <mjacques@wpi.edu>
2023-04-03 00:03:41 -0400

    MealRequest created & worked on
    Not working yet. Hope to be done by meeting/halfway through meeting tomorrow

--- src/main/java/edu/wpi/teame/controllers/MealRequestController.java
+++ src/main/java/edu/wpi/teame/controllers/MealRequestController.java
@@ -3,33 +3,50 @@ package edu.wpi.teame.controllers;
 import edu.wpi.teame.navigation.Navigation;
 import edu.wpi.teame.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXComboBox;
 import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import org.json.JSONObject;
 
 public class MealRequestController implements IRequestController {
-  @FXML MFXButton backButton;
+  @FXML MFXButton returnButtonMealRequest;
   @FXML MFXButton cancelButton;
   @FXML MFXButton submitButton;
-  @FXML MFXTextField deliveryTime;
-  @FXML MFXTextField foodSelection;
+  @FXML MFXComboBox deliveryTime;
   @FXML MFXTextField notes;
-  @FXML MFXTextField receiverName;
+  @FXML MFXTextField recipientName;
   @FXML MFXTextField roomNumber;
-  @FXML MFXTextField sideSelection;
+  @FXML MFXComboBox mainCourseChoice;
+  @FXML MFXComboBox sideCourseChoice;
+
+  ObservableList<String> deliveryTimes =
+      FXCollections.observableArrayList(
+          "10am - 11am", "11am - 12pm", "12pm - 1pm", "1pm - 2pm", "2pm - 3pm", "3pm - 4pm");
+
+  ObservableList<String> mainCourses =
+      FXCollections.observableArrayList(
+          "Hamburger", "Cheeseburger", "Grilled Cheese", "Chicken Nuggets");
+  ObservableList<String> sideCourses =
+      FXCollections.observableArrayList("Fries", "Apple Slices", "Tater Tots", "Carrots");
 
   @FXML
   public void initialize() {
-    backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SERVICE_REQUESTS));
+    mainCourseChoice.setItems(mainCourses);
+    sideCourseChoice.setItems(sideCourses);
+    deliveryTime.setItems(deliveryTimes);
+    returnButtonMealRequest.setOnMouseClicked(
+        event -> Navigation.navigate(Screen.SERVICE_REQUESTS));
     cancelButton.setOnMouseClicked(event -> cancelRequest());
     submitButton.setOnMouseClicked(event -> sendRequest());
   }
 
   public ServiceRequestData sendRequest() {
     // Get the selected/input values
-    String selCourse = foodSelection.getText();
-    String selSide = sideSelection.getText();
-    String inputName = receiverName.getText();
+    String selCourse = mainCourseChoice.getText();
+    String selSide = sideCourseChoice.getText();
+    String inputName = recipientName.getText();
     String inputDelivery = deliveryTime.getText();
     String inputRoom = roomNumber.getText();
     String inputNotes = notes.getText();
