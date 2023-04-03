0cc24d40bbc3bbff11805ced60e5f726328bc11a
Seth Frank <sbfrank@wpi.edu>
2023-04-03 13:51:02 -0400

    Set up initial page structure for map page

--- src/main/java/edu/wpi/teamA/controllers/Navigation/MapController.java
+++ src/main/java/edu/wpi/teamA/controllers/Navigation/MapController.java
@@ -2,9 +2,14 @@ package edu.wpi.teamA.controllers.Navigation;
 
 import edu.wpi.teamA.navigation.Navigation;
 import edu.wpi.teamA.navigation.Screen;
+import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
 
 public class MapController implements IPageController {
+
+  // @FXML private TableView mapData;
+  @FXML private MFXButton importButton;
+  @FXML private MFXButton exportButton;
 
   @FXML
   public void initialize() {}
