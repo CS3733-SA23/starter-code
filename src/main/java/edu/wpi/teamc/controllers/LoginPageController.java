a03caeb488881f70874df510830065809e9add6f
Sebastian Valle <svalle@wpi.edu>
2023-04-03 18:56:51 -0400

    added Login page and menu bar for home page

---
+++ src/main/java/edu/wpi/teamc/controllers/LoginPageController.java
@@ -0,0 +1,17 @@
+package edu.wpi.teamc.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LoginPageController {
+
+    @FXML
+    private Button Map;
+
+    @FXML
+    void getMapPage(ActionEvent event) {
+
+    }
+
+}
