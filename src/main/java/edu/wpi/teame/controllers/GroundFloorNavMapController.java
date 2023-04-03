50482e59857b15523d88c675313a5d684e7c51d2
DiyarJaafar <116843342+DiyarJaafar@users.noreply.github.com>
2023-04-03 16:14:50 -0400

    Update GroundFloorNavMapController.java

--- src/main/java/edu/wpi/teame/controllers/GroundFloorNavMapController.java
+++ src/main/java/edu/wpi/teame/controllers/GroundFloorNavMapController.java
@@ -4,6 +4,7 @@ import edu.wpi.teame.navigation.Navigation;
 import edu.wpi.teame.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXComboBox;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 
 public class GroundFloorNavMapController {
@@ -20,6 +21,8 @@ public class GroundFloorNavMapController {
   @FXML private MFXButton lowerLevelTwoButton;
   @FXML MFXComboBox currentLocationList;
   @FXML MFXComboBox destinationList;
+  ObservableList<String> floorLocations;
+
   @FXML
   public void initialize() {
     backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
@@ -28,5 +31,6 @@ public class GroundFloorNavMapController {
     firstFloorButton.setOnMouseClicked(event -> Navigation.navigate(Screen.FLOOR_ONE));
     secondFloorButton.setOnMouseClicked(event -> Navigation.navigate(Screen.FLOOR_TWO));
     thirdFloorButton.setOnMouseClicked(event -> Navigation.navigate(Screen.FLOOR_THREE));
+
   }
 }
