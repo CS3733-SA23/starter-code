71185391c6a74dfcf2426b98d50b58ada3322c3a
C-beaus <cabeausoleil@wpi.edu>
2023-04-03 22:53:07 -0400

    Created a new class and edited the MapHistory controller to allow for data to be taken from a variable in the project and outputted to the UI of the app in a FilteredTableView object with a scroll pane

--- src/main/java/edu/wpi/teamc/controllers/MapChangeHistoryController.java
+++ src/main/java/edu/wpi/teamc/controllers/MapChangeHistoryController.java
@@ -3,9 +3,12 @@ package edu.wpi.teamc.controllers;
 import edu.wpi.teamc.navigation.Navigation;
 import edu.wpi.teamc.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.controlsfx.control.tableview2.FilteredTableView;
 
 public class MapChangeHistoryController {
 
@@ -20,6 +23,31 @@ public class MapChangeHistoryController {
 
   @FXML private Button testButton;
   @FXML private TextField inputBox;
+  @FXML private FilteredTableView<TableRow> historyTable;
+  @FXML TableView<TableRow> otherTable;
+  @FXML TableColumn<TableRow, String> ColumnOne;
+  @FXML TableColumn<TableRow, String> ColumnTwo;
+  @FXML TableColumn<TableRow, String> ColumnThree;
+  ObservableList<TableRow> rows = FXCollections.observableArrayList();
+
+  public void dispTable(javafx.event.ActionEvent actionEvent) {
+    ColumnOne.setCellValueFactory(new PropertyValueFactory<TableRow, String>("nodeID"));
+    ColumnTwo.setCellValueFactory(new PropertyValueFactory<TableRow, String>("longName"));
+    ColumnThree.setCellValueFactory(new PropertyValueFactory<TableRow, String>("date"));
+    historyTable.getItems().setAll(gettableRows());
+
+    System.out.println("did it");
+  }
+
+  public ObservableList<TableRow> gettableRows() {
+    rows.add(new TableRow("1234", "Alpha", "4/17/02"));
+    rows.add(new TableRow("456", "Beta", "4/5/2023"));
+    return rows;
+  }
+  //  @Override
+  //  public void initialize(URL location, ResourceBundle resources) {
+  //
+  //  }
 
   //  @FXML
   //  public String getText(ActionEvent event) {
@@ -33,4 +61,9 @@ public class MapChangeHistoryController {
     inputtedText = inputBox.getText();
     return inputtedText;
   }
+  //  public void createTable() {
+  //    String test = "testing";
+  //    ColLocation = ;
+  //
+  //  }
 }
