269be83a2c7bc6b8495dab35d606a8c599411b29
Alton Miles <altonmiles@vpnclient-10-217-65-11.int.wpi.edu>
2023-04-04 00:39:08 -0400

    Fixed EdgeDAOTest to make sure that the nodes and edges are updated properly between tests (made the class pass)

--- src/test/java/edu/wpi/teamR/database/EdgeDAOTest.java
+++ src/test/java/edu/wpi/teamR/database/EdgeDAOTest.java
@@ -4,11 +4,8 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.LinkedList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,55 +20,52 @@ class EdgeDAOTest {
         String password = "teamr150";
         String schemaName = "test";
         String url = "jdbc:postgresql://database.cs.wpi.edu:5432/teamrdb";
-        Class.forName("org.postgresql.Driver");
-        Connection conn = DriverManager.getConnection(url, username, password);
-        conn.createStatement().executeUpdate("DELETE FROM test.edge;");
 
         //https://mega.nz/file/OQgCzZyS#hCwVqtJTGRmi3atPbmG1XLeR85FRZ4bOULGc8JpKULw
         nodeDAO = NodeDAO.createInstance(username, password, "node", schemaName, url);
         edgeDAO = EdgeDAO.createInstance(username, password, "edge", schemaName, url);
-        nodeDAO.addNode(1, 0, 0, "", "");
-        nodeDAO.addNode(2, 1, 1, "", "");
-        nodeDAO.addNode(3, 2, 0, "", "");
-        nodeDAO.addNode(4, 1, -1, "", "");
-        nodeDAO.addNode(5, 1, 0, "", "");
-        nodeDAO.addNode(6, 3, 0, "", "");
-        edgeDAO.addEdge(1, 2);
-        edgeDAO.addEdge(1, 4);
-        edgeDAO.addEdge(1, 5);
-        edgeDAO.addEdge(2, 5);
-        edgeDAO.addEdge(5, 4);
-        edgeDAO.addEdge(2, 3);
-        edgeDAO.addEdge(5, 3);
-        edgeDAO.addEdge(4, 3);
-        edgeDAO.addEdge(3, 6);
+        resetNodes();
     }
 
     @AfterAll
     static void tearDown() throws Exception {
+        for (Node node : nodeDAO.getNodes()){
+            edgeDAO.deleteAllEdges(node.getNodeID());
+        }
         nodeDAO.deleteNodes(null, null, null, null, null);
     }
 
     @Test
-    void getEdges() {
+    void getEdges() throws SQLException, ClassNotFoundException {
+        resetNodes();
         assertEquals(edgeDAO.getEdges().size(), 9);
     }
 
     @Test
     void addEdgeAndDeleteConnectingEdge() throws SQLException, ClassNotFoundException {
+        resetNodes();
+
+        assertEquals(edgeDAO.getAdjacentNodeIDs(6).size(), 1);
         edgeDAO.addEdge(2, 6);
         edgeDAO.addEdge(4, 6);
+        assertEquals(edgeDAO.getAdjacentNodeIDs(6).size(), 3);
         edgeDAO.deleteConnectingEdge(2, 6);
         edgeDAO.deleteConnectingEdge(4, 6);
+        assertEquals(edgeDAO.getAdjacentNodeIDs(6).size(), 1);
 
         edgeDAO.addEdge(6, 2);
         edgeDAO.addEdge(6, 4);
+        assertEquals(edgeDAO.getAdjacentNodeIDs(6).size(), 3);
         edgeDAO.deleteConnectingEdge(2, 6);
+        assertEquals(edgeDAO.getAdjacentNodeIDs(6).size(), 2);
         edgeDAO.deleteConnectingEdge(4, 6);
+        assertEquals(edgeDAO.getAdjacentNodeIDs(6).size(), 1);
     }
 
     @Test
-    void getAdjacentNodeIDs() {
+    void getAdjacentNodeIDs() throws SQLException, ClassNotFoundException {
+        resetNodes();
+
         ArrayList<Integer> nodes;
         nodes = edgeDAO.getAdjacentNodeIDs(6);
         assertEquals(nodes.size(), 1);
@@ -94,6 +88,8 @@ class EdgeDAOTest {
 
     @Test
     void deleteAllEdges() throws SQLException, ClassNotFoundException {
+        resetNodes();
+
         ArrayList<Integer> nodes;
 
         edgeDAO.deleteAllEdges(5);
@@ -106,8 +102,31 @@ class EdgeDAOTest {
         nodes = edgeDAO.getAdjacentNodeIDs(6);
         assertEquals(nodes.size(), 0);
 
+        edgeDAO.deleteAllEdges(1);
         nodeDAO.deleteNodes(1, null, null, null, null);
         nodes = edgeDAO.getAdjacentNodeIDs(4);
         assertEquals(nodes.size(), 0);
+    }
+
+    private static void resetNodes() throws SQLException, ClassNotFoundException {
+        for (Node node : nodeDAO.getNodes()){
+            edgeDAO.deleteAllEdges(node.getNodeID());
+        }
+        nodeDAO.deleteNodes(null, null, null, null, null);
+        nodeDAO.addNode(1, 0, 0, "", "");
+        nodeDAO.addNode(2, 1, 1, "", "");
+        nodeDAO.addNode(3, 2, 0, "", "");
+        nodeDAO.addNode(4, 1, -1, "", "");
+        nodeDAO.addNode(5, 1, 0, "", "");
+        nodeDAO.addNode(6, 3, 0, "", "");
+        edgeDAO.addEdge(1, 2);
+        edgeDAO.addEdge(1, 4);
+        edgeDAO.addEdge(1, 5);
+        edgeDAO.addEdge(2, 5);
+        edgeDAO.addEdge(5, 4);
+        edgeDAO.addEdge(2, 3);
+        edgeDAO.addEdge(5, 3);
+        edgeDAO.addEdge(4, 3);
+        edgeDAO.addEdge(3, 6);
     }
 }
