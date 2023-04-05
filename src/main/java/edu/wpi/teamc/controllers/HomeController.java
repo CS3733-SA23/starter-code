708b7576678ae6f898cfb52fd92c67d89170f10b
azzhang3 <azhang3@wpi.edu>
2023-04-05 12:42:41 -0400

    Added Temporary MenuBar for Map Page - Aaron

--- src/main/java/edu/wpi/teamc/controllers/HomeController.java
+++ src/main/java/edu/wpi/teamc/controllers/HomeController.java
@@ -111,7 +111,9 @@ public class HomeController {
   }
 
   @FXML
-  void getMapPage(ActionEvent event) {}
+  void getMapPage(ActionEvent event) {
+    Navigation.navigate(Screen.FLOOR_PLAN);
+  }
 
   @FXML
   void getImportNodes(ActionEvent event) {
