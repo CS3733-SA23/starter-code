16daec708b8b8aa1f4cf5312712bfc2d8683869e
alex <ajsamra@wpi.edu>
2023-04-05 15:50:11 -0400

    added conference room request object

--- src/main/java/edu/wpi/teamA/database/ORMclasses/ConferenceRoomResRequest.java
+++ src/main/java/edu/wpi/teamA/database/ORMclasses/ConferenceRoomResRequest.java
@@ -1,26 +1,25 @@
 package edu.wpi.teamA.database.ORMclasses;
 
+import java.sql.Date;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.sql.Date;
-
 public class ConferenceRoomResRequest {
-    @Getter @Setter private String name;
-    @Getter @Setter private int room;
-    @Getter @Setter private Date date;
-    @Getter @Setter private int startTime;
-    @Getter @Setter private int endTime;
-    @Getter @Setter private String comment;
-    @Getter @Setter private String status;
+  @Getter @Setter private String name;
+  @Getter @Setter private int room;
+  @Getter @Setter private Date date;
+  @Getter @Setter private int startTime;
+  @Getter @Setter private int endTime;
+  @Getter @Setter private String comment;
+  @Getter @Setter private String status;
 
-    public ConferenceRoomResRequest(String a, int b, Date c, int d, int e, String f) {
-        this.name = a;
-        this.room = b;
-        this.date = c;
-        this.startTime = d;
-        this.endTime = e;
-        this.comment = f;
-        this.status = "new";
-    }
+  public ConferenceRoomResRequest(String a, int b, Date c, int d, int e, String f) {
+    this.name = a;
+    this.room = b;
+    this.date = c;
+    this.startTime = d;
+    this.endTime = e;
+    this.comment = f;
+    this.status = "new";
+  }
 }
