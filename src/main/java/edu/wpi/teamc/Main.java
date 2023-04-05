fcdbbdb913efc905135e66094096884a04981c4a
C-beaus <cabeausoleil@wpi.edu>
2023-04-04 23:18:07 -0400

    Fixed some stuff

--- src/main/java/edu/wpi/teamc/Main.java
+++ src/main/java/edu/wpi/teamc/Main.java
@@ -9,32 +9,12 @@ public class Main {
 
   public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-    Scanner scanner = new Scanner(System.in);
-
-    Graph graph = new Graph();
-    //    List<Node> databaseNodeList = new ArrayList<Node>();
-    //    List<Edge> databaseEdgeList = new ArrayList<Edge>();
-    //    List<LocationName> databaseLocationNameList = new ArrayList<LocationName>();
-    //    List<Move> databaseMoveList = new ArrayList<Move>();
-    try {
-      graph.init();
-    } catch (IOException e) {
-      System.out.println("Exception!");
-    }
-    // graph.syncWithDB();
-
-    String startNode = "CCONF001L1";
-    String endNode = "CHALL010L1";
-
-    // graph.printDirectionsAStar(startNode, endNode);
     Cdb.loadDatabaseTables(
         Cdb.databaseNodeList,
         Cdb.databaseEdgeList,
         Cdb.databaseLocationNameList,
         Cdb.databaseMoveList);
-    //    Cdb.displayMoveInfo(databaseMoveList);
 
     CApp.launch(CApp.class, args);
-    // graph.printDirections("CCONF001L1", "CHALL010L1");
   }
 }
