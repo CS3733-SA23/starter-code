c1b550f0ce383061ea5ca088722b21058b30a9f9
DiyarJaafar <116843342+DiyarJaafar@users.noreply.github.com>
2023-04-04 20:15:47 -0400

    fixed main

--- src/main/java/edu/wpi/teame/Main.java
+++ src/main/java/edu/wpi/teame/Main.java
@@ -7,7 +7,7 @@ import edu.wpi.teame.map.HospitalNode;
 public class Main {
 
   public static void main(String[] args) {
-    DatabaseController db = DatabaseController.INSTANCE;
+    DatabaseController db = new DatabaseController("teame", "teame50");
     DatabaseGraphController graphController = new DatabaseGraphController(db);
     graphController.retrieveFromTable();
     HospitalNode.processEdgeList(graphController.getHospitalEdges());
