ffe49bf7d5f0a705e7f311c96da76f127fe0b5dc
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2023-04-05 00:08:09 -0400

    change requests

--- src/main/java/edu/wpi/teamc/serviceRequest/Meal.java
+++ src/main/java/edu/wpi/teamc/serviceRequest/Meal.java
@@ -4,17 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 public class Meal {
-  @Getter
-  @Setter
-  private String mealName;
-@Getter
-@Setter
-  private String addtionalInfo;
+  @Getter @Setter private String mealName;
+  @Getter @Setter private String addtionalInfo;
 
   // Should it be this.mealName = mealName? and this.addtionalInfo = addtionalInfo?
   public Meal(String name, String notes) {
     this.mealName = name;
     this.addtionalInfo = notes;
   }
-
 }
