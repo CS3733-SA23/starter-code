a4ff3ceaaf399761dd471efde006673727ce33e0
tyakovenko <taisia.yakovenko@gmail.com>
2023-04-04 20:54:29 -0400

    addMealRequest, addConferenceRoomRequest, updateMealRequest, updateConferenceRoomRequest written; last 3 need to be tested. for meal req, need to make sure to set class not string

--- src/main/java/edu/wpi/teamc/serviceRequest/ConferenceRoomRequest.java
+++ src/main/java/edu/wpi/teamc/serviceRequest/ConferenceRoomRequest.java
@@ -10,19 +10,66 @@ public class ConferenceRoomRequest implements IServiceRequest {
   private Date date;
 
   private String startTime;
-
   private String endTime;
-
   private String addtionalNotes;
+  private STATUS stat;
 
   public ConferenceRoomRequest(
-      ConferenceRoom select, Date date, String startTime, String endTime, String addtionalNotes) {
+      ConferenceRoom select,
+      Date date,
+      String startTime,
+      String endTime,
+      String addtionalNotes,
+      STATUS stat) {
     selection = select;
     this.date = date;
     this.startTime = startTime;
     this.endTime = endTime;
     this.addtionalNotes = addtionalNotes;
+    this.stat = stat;
   }
 
-  // need to change availability to false
+  public ConferenceRoom getSelection() {
+    return selection;
+  }
+
+  public void setSelection(ConferenceRoom selection) {
+    this.selection = selection;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  public String getStartTime() {
+    return startTime;
+  }
+
+  public void setStartTime(String startTime) {
+    this.startTime = startTime;
+  }
+
+  public String getEndTime() {
+    return endTime;
+  }
+
+  public void setEndTime(String endTime) {
+    this.endTime = endTime;
+  }
+
+  public String getAddtionalNotes() {
+    return addtionalNotes;
+  }
+
+  public void setAddtionalNotes(String addtionalNotes) {
+    this.addtionalNotes = addtionalNotes;
+  }
+
+  public String getStat() {
+    return stat.name();
+  }
 }
