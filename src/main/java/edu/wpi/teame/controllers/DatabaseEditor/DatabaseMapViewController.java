d40808477f811328e743fe05c9a5a6777b3e4aa9
michibatoryu <114957192+michibatoryu@users.noreply.github.com>
2023-04-10 16:56:23 -0400

    stand-in method for viewing node metadata

--- src/main/java/edu/wpi/teame/controllers/DatabaseEditor/DatabaseMapViewController.java
+++ src/main/java/edu/wpi/teame/controllers/DatabaseEditor/DatabaseMapViewController.java
@@ -1,9 +1,36 @@
 package edu.wpi.teame.controllers.DatabaseEditor;
 
+import edu.wpi.teame.map.HospitalNode;
+import edu.wpi.teame.utilities.MapUtilities;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
 
 public class DatabaseMapViewController {
 
+  @FXML AnchorPane mapPane;
+  MapUtilities util = new MapUtilities();
+
   @FXML
-  public void initialize() {}
+  public void initialize() {
+    editableNode(new HospitalNode());
+  }
+
+  private void editableNode(HospitalNode node) {
+    Circle nodePoint = util.drawHospitalNode(node, mapPane);
+    nodePoint.setOnMouseClicked(
+        new EventHandler<MouseEvent>() {
+          @Override
+          public void handle(MouseEvent event) {
+            displayMetadata(node);
+          }
+        });
+  }
+
+  private void displayMetadata(HospitalNode node) {
+    // activate side panel with correct data
+    System.out.println(node);
+  }
 }
